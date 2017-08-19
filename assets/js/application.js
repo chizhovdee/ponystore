64f15581485ff49451576066cb645a17ba82cace
Dmitry Chizhov <chizhov.dee@gmail.com>
2017-08-19 15:32:29 +0500

    added undersocre, added application module

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,30 @@
+var App = (function(){
+    function App(){
+        this.el = document.getElementsByClassName("catalog")[0];
+        this.template = _.template(document.getElementById('catalog-template').innerHTML);
 
+        this.bindEventListeners();
+
+        var defaultData = _.sample(appData, 20);
+
+        this.render({items: defaultData});
+    }
+
+    App.prototype = {
+      render: function(data){
+          this.el.innerHTML = this.template(data);
+      },
+
+      bindEventListeners: function(){
+          document.getElementById('btn-filter').addEventListener('click', function(e){
+              console.log(e);
+          });
+      }
+    };
+
+    return App;
+})();
+
+ready(function(){
+    new App();
+});
